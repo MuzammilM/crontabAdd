15d17935d007b0370c44eefd43ca1ab2783bdb1e
Muzammil Hasan Momin <muzammilmomin@live.com>
2018-03-14 15:26:41 +0530

    Update crontabAdd.sh

--- crontabAdd.sh
+++ crontabAdd.sh
@@ -1,3 +1,15 @@
+#Author : MuzammilM
+#Adds an entry into crontab
+COLOR='\033[0;31m'
+reset=`tput sgr0`
+
+if [ $# -lt 1 ]
+then
+        echo -e "${COLOR}No arguments passed."
+        echo -e "${COLOR}Adds an entry into crontab"
+        echo -e "${COLOR}Execution : crontabAdd "* * * * * echo input2"
+        exit
+fi
 read -p "Please enter appropriate cron comment: " userInput
 if [[ -z "$userInput" ]]; then
         mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo "${@}" >> crontab && crontab crontab && crontab -l
