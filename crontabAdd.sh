4ab2fa4854ce8d3366886062ffb0c5a4d5aba5f2
Muzammil Hasan Momin <muzammilmomin@live.com>
2018-04-15 17:55:46 +0530

    Update crontabAdd.sh

--- crontabAdd.sh
+++ crontabAdd.sh
@@ -12,9 +12,11 @@ then
 fi
 read -p "Please enter appropriate cron comment: " userInput
 if [[ -z "$userInput" ]]; then
+        mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo -e "\n" >> crontab && crontab crontab && crontab -l
         mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo "${@}" >> crontab && crontab crontab && crontab -l
         exit 1
 else
+        mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo -e "\n" >> crontab && crontab crontab && crontab -l
         mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo \#"$userInput" >> crontab && crontab crontab && crontab -l
         mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo "${@}" >> crontab && crontab crontab && crontab -l
 fi
