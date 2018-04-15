b060d503a37ffe2a6283649db556a7641cfa1d87
Muzammil Hasan Momin <muzammilmomin@live.com>
2018-04-16 03:12:25 +0530

    Update crontabAdd.sh

--- crontabAdd.sh
+++ crontabAdd.sh
@@ -10,13 +10,17 @@ then
         echo -e "${COLOR}Execution : crontabAdd \"* * * * * echo input2\" ${reset}"
         exit
 fi
+list=` echo \`crontab -l | grep id | awk '{print $1}' \` | sed 's/#id//g'`
+last="$(printf "%d\n" ${list[@]} | sort -n | tail -n1)"
+new=$(($last+1))
 read -p "Please enter appropriate cron comment: " userInput
 if [[ -z "$userInput" ]]; then
         mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo -e "\n" >> crontab && crontab crontab && crontab -l
+        mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo -e \#"id"$new >> crontab && crontab crontab && crontab -l
         mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo "${@}" >> crontab && crontab crontab && crontab -l
         exit 1
 else
         mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo -e "\n" >> crontab && crontab crontab && crontab -l
-        mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo \#"$userInput" >> crontab && crontab crontab && crontab -l
+        mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo \#id"$new ""$userInput" >> crontab && crontab crontab && crontab -l
         mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo "${@}" >> crontab && crontab crontab && crontab -l
 fi
