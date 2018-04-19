ae961edaec7fd14eeb3545994a8cf337ce505bc2
Muzammil Hasan Momin <muzammilmomin@live.com>
2018-04-19 16:54:07 +0530

    Update crontabAdd.sh

--- crontabAdd.sh
+++ crontabAdd.sh
@@ -1,4 +1,3 @@
-mpuser@alpha:~$ more bin/shellscript/crontabAdd.sh
 #Author : MuzammilM
 #Adds an entry into crontab
 COLOR='\033[0;31m'
@@ -6,22 +5,20 @@ reset=`tput sgr0`
 
 if [ $# -lt 1 ]
 then
-        echo -e "${COLOR}No arguments passed."
-        echo -e "${COLOR}Adds an entry into crontab"
-        echo -e "${COLOR}Execution : crontabAdd \"* * * * * echo input2\" ${reset}"
+        /bin/echo -e "${COLOR}No arguments passed."
+        /bin/echo -e "${COLOR}Adds an entry into crontab"
+        /bin/echo -e "${COLOR}Execution : crontabAdd \"* * * * * /bin/echo input2\" ${reset}"
         exit
 fi
-list=` echo \`crontab -l | grep id | awk '{print $1}' \` | sed 's/#id//g'`
-last="$(printf "%d\n" ${list[@]} | sort -n | tail -n1)"
+last="$(/usr/bin/printf "%d\n" `/usr/bin/crontab -l | /bin/grep id | /usr/bin/awk '{print $1}' | /bin/sed 's/#id//g'` | /usr/bin/sort -n | /usr/bin/tail -n1)"
 new=$(($last+1))
 read -p "Please enter appropriate cron comment: " userInput
 if [[ -z "$userInput" ]]; then
-        mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo -e "\n" >> crontab && crontab crontab && crontab -l
-        mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo -e \#"id"$new >> crontab && crontab crontab && crontab -l
-        mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo "${@}" >> crontab && crontab crontab && crontab -l
+        /bin/mv crontab crontab.backup || /bin/mkdir -p /home/$USER/logs/cronLogs/ && /usr/bin/crontab -l > crontab && /bin/echo -e "\n" >> crontab && /bin/echo -e \#"id"$new >> crontab && /bin/echo "${@}" >> crontab && /usr/bin/crontab crontab
+        /usr/bin/crontab -l
         exit 1
 else
-        mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo -e "\n" >> crontab && crontab crontab && crontab -l
-        mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo \#id"$new ""$userInput" >> crontab && crontab crontab && crontab -l
-        mv crontab crontab.backup || mkdir -p ~/logs/cronLogs/ && crontab -l > crontab && echo "${@}" >> crontab && crontab crontab && crontab -l
+        /bin/mv crontab crontab.backup || /bin/mkdir -p /home/$USER/logs/cronLogs/ && crontab -l > crontab && /bin/echo -e "\n" >> crontab && /bin/echo -e \#"id"$new " "$userInput >> crontab && /bin/echo "${@}" >> crontab && crontab crontab
+        /usr/bin/crontab -l
+        exit
 fi
